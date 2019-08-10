223b2bc4dd3d36eb46e2f6e4d851f408156131db
Tyler Eads <tyler.eads9@gmail.com>
2019-08-10 12:32:54 -0400

    First build

--- src/controllers/crypto/MarketController.ts
+++ src/controllers/crypto/MarketController.ts
@@ -14,6 +14,7 @@ class MarketController {
         const { path, router, ...x } = this;
         router.get(`${path}/omc`, x.omc);
         router.get(`${path}/cmc`, x.cmc);
+        router.get(`${path}/cmcEdited`, x.cmcEdited);
     }
 
     omc = async (req: express.Request, res: express.Response) => {
@@ -31,6 +32,28 @@ class MarketController {
             const cmc = new CoinMarketCapService();
             const tokens = await cmc.getTopTenTokens(10, 'USD');
             res.json(tokens);
+        } catch (error) {
+            throw new Error('CoinMarketCap');
+        }
+    };
+
+    cmcEdited = async (req: express.Request, res: express.Response) => {
+        try {
+            const cmc = new CoinMarketCapService();
+            const tokens = await cmc.getTopTenTokens(10, 'USD');
+
+            const query = (x: any) => {
+                return {
+                    symbol: x.symbol,
+                    name: x.name,
+                    rank: x.cmc_rank,
+                    price: x.quote.USD.price.toFixed(2) || null
+                };
+            };
+
+            const payload = [...tokens.data].map(query);
+
+            res.json(payload);
         } catch (error) {
             throw new Error('CoinMarketCap');
         }
