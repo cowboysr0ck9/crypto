b5199a55d4a1c1ada51002a1ab838b4df2d8f5f2
Tyler Eads <tylereads@Tylers-MacBook-Pro.local>
2020-08-08 12:06:00 -0400

    Updated Config

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,5 +3,5 @@ dotenv.config();
 
 export const APP_CONFIG: any = {
     COIN_MARKET_CAP_KEY: process.env.COIN_MARKET_CAP_KEY,
-    PORT: Number(process.env.PORT)
+    PORT: Number(process.env.PORT) || 5000,
 };
